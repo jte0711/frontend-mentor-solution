91f8634b27db38c517a7100900ca889797c958e6
James <james.tedy@ymail.com>
2022-08-20 20:28:28 +1000

    Remove uneccessary details

--- pages/index.js
+++ pages/index.js
@@ -18,38 +18,7 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col justify-center items-center h-screen ">
-        <p className="font-semibold text-5xl font-mono text-center">
-          Hi, I'm James
-        </p>
-        <p className="text-lg font-mono max-w-3xl mt-8 text-center">
-          I'm Software Engineer living in Jakarta, Indonesia. I'm interested in
-          AI, Blockchain, and Mobile App.
-        </p>
-        <div className="flex w-full h-5 justify-center mt-5">
-          <Link href={"https://github.com/jte0711"} passHref>
-            <a target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faGithub} className="text-xl" />
-            </a>
-          </Link>
-
-          <Link
-            href={"https://stackoverflow.com/users/7870211/jted95"}
-            passHref
-          >
-            <a target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon
-                icon={faStackOverflow}
-                className="text-xl ml-4"
-              />
-            </a>
-          </Link>
-
-          <Link href={"https://twitter.com/jmstdy95"} passHref>
-            <a target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faTwitter} className="text-xl ml-4" />
-            </a>
-          </Link>
-        </div>
+        
       </main>
     </div>
   );
