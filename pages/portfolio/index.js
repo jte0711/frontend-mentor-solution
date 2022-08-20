a861461576b676ff5e41e312824520b1132333f4
James Tedy <james.tedy@ymail.com>
2022-08-20 17:25:40 +0700

    WIP Portfolio Page

---
+++ pages/portfolio/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content="James' Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default Portfolio;
